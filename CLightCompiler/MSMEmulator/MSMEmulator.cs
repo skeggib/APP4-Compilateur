dd618d4c31116fe6bf30ce1d09000396bae83890
Sébastien Klasa <klasa.sebastien@gmail.com>
2017-11-23 22:17:30 +0100

    [MSMEmulator] Add memory

--- CLightCompiler/MSMEmulator/MSMEmulator.cs
+++ CLightCompiler/MSMEmulator/MSMEmulator.cs
@@ -1,14 +1,64 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MSMEmulator
 {
+    public enum Instruction : byte
+    {
+        Halt,
+        Drop, Dup, Pushi, Pushf,
+        Get, Set, Read, Write,
+        Addi, Subi, Muli, Divi,
+        Modi, Addf, Subf, Mulf,
+        Divf, Not, And, Or,
+        Itof, Ftoi, Cmpeqi, Cmpnei,
+        Cmplti, Cmplei, Cmpgti, Cmpgei,
+        Cmpeqf, Cmpnef, Cmpltf, Cmplef,
+        Cmpgtf, Cmpgef, Jump, Jumpt,
+        Jumpf, Prep, Call, Ret,
+        Outi, Outf, Outc
+    }
+
     public class MSMEmulator
     {
-        public string Run(string asm)
+        private Tuple<Instruction, int>[] _memory;
+        private long _programIndex;
+        private long _stackIndex;
+
+        public MSMEmulator(long memorySize = 2^16)
+        {
+            _memory = new Tuple<Instruction, int>[memorySize];
+            _programIndex = 0;
+            _stackIndex = _memory.Length - 1;
+        }
+
+        public void Load(string asm)
+        {
+            string[] lines = asm.Split('\n');
+            foreach (var line in lines)
+            {
+                string[] commentSplit = line.Split(';');
+                if (commentSplit.Length > 0)
+                {
+                    string instruction = commentSplit[0];
+                    string[] keywordSplit = instruction.Split(' ');
+                    if (keywordSplit.Length > 0)
+                    {
+                        string keyword = keywordSplit[0];
+
+                        switch (keyword)
+                        {
+                            case "halt":
+                                _memory[_programIndex++] = new Tuple<Instruction, int>(Instruction.Halt, 0);
+                                break;
+                            default:
+                                break;
+                        }
+                    }
+                }
+            }
+        }
+
+        public string Run()
         {
             return null;
         }
